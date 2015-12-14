0791aea2f8302c2fe5c81d3964a45b2fda9c5a5b
Libo Chen <chenlibo0928@gmail.com>
2015-12-14 00:38:22 -0600

    impleneted view nearby POI

--- AndroidDemo/app/src/main/java/com/aptPhase3/zhiyuan/android/ViewNearbyActivity.java
+++ AndroidDemo/app/src/main/java/com/aptPhase3/zhiyuan/android/ViewNearbyActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 //import com.aptdemo.yzhao.androiddemo.R;
@@ -34,7 +35,7 @@ public class ViewNearbyActivity extends ActionBarActivity
     Context context = this;
     protected MyApplication myApp;
     private GoogleApiClient geoClient;
-    private String TAG  = "View Nearby Streams";
+    private String TAG  = "View Nearby POI";
 
 
     @Override
@@ -72,48 +73,66 @@ public class ViewNearbyActivity extends ActionBarActivity
             Toast.makeText(context, "Failed to retrieve location", Toast.LENGTH_SHORT).show();
         }
         System.out.println(request_url);
+        request_url = request_url.replace(" ", "%20");
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(request_url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                 System.out.println("Post successful!!!!!!!");
-                final ArrayList<String> imageURLs = new ArrayList<String>();
-                final ArrayList<String> allStreamId = new ArrayList<String>();
+                final ArrayList<String> streamInfos = new ArrayList<String>();
+                final ArrayList<String> streamNames = new ArrayList<String>();
+                final ArrayList<String> streamCovers = new ArrayList<String>();
                 try {
                     JSONObject jObject = new JSONObject(new String(response));
                     JSONArray displayImages = jObject.getJSONArray("displayImages");
-//                    JSONArray displayStreamId = jObject.getJSONArray("streamId");
                     for (int i = 0; i < displayImages.length(); i++) {
                         JSONObject singleImage = new JSONObject(displayImages.getString(i));
-                        imageURLs.add(singleImage.getString("url"));
-                        allStreamId.add(singleImage.getString("stream_id"));
-//                        imageURLs.add(displayImages.getString(i));
-//                        allStreamId.add(displayStreamId.getString(i));
-//                        System.out.println(displayImages.getString(i));
+                        streamCovers.add(singleImage.getString("url"));
+                        streamNames.add(singleImage.getString("stream_id"));
+                        streamInfos.add(singleImage.getString("description"));
                     }
-                    GridView gridview = (GridView) findViewById(R.id.gridview);
-                    gridview.setAdapter(new ImageAdapter(context, imageURLs));
-                    gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View v,
-                                                int position, long id) {
 
-//                            Toast.makeText(context, imageCaps.get(position), Toast.LENGTH_SHORT).show();
-//
-//                            Dialog imageDialog = new Dialog(context);
-//                            imageDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//                            imageDialog.setContentView(R.layout.thumbnail);
-//                            ImageView image = (ImageView) imageDialog.findViewById(R.id.thumbnail_IMAGEVIEW);
-//
-//                            Picasso.with(context).load(imageURLs.get(position)).into(image);
-//
-//                            imageDialog.show();
-                            String stream_id = allStreamId.get(position);
+                    ArrayList<Stream> stream_data = new ArrayList<Stream>();
+                    for(int i = 0; i < streamNames.size(); i++){
+                        Stream stream = new Stream(streamNames.get(i), streamInfos.get(i), streamCovers.get(i));
+                        stream_data.add(stream);
+                    }
+
+                    ListView listview = (ListView) findViewById(R.id.listViewStream);
+                    final StreamAdapter adapter = new StreamAdapter(context, R.layout.listview_stream_row, stream_data);
+                    listview.setAdapter(adapter);
+                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                            String streamName = streamNames.get(position);
                             Intent i = new Intent(getApplicationContext(), ViewAStreamActivity.class);
-                            i.putExtra("stream_id", stream_id);
+                            i.putExtra("stream_id", streamName);
                             startActivity(i);
                         }
                     });
+
+//                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                        @Override
+//                        public void onItemClick(AdapterView<?> parent, View v,
+//                                                int position, long id) {
+//
+////                            Toast.makeText(context, imageCaps.get(position), Toast.LENGTH_SHORT).show();
+////
+////                            Dialog imageDialog = new Dialog(context);
+////                            imageDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+////                            imageDialog.setContentView(R.layout.thumbnail);
+////                            ImageView image = (ImageView) imageDialog.findViewById(R.id.thumbnail_IMAGEVIEW);
+////
+////                            Picasso.with(context).load(imageURLs.get(position)).into(image);
+////
+////                            imageDialog.show();
+//                            String stream_id = allStreamId.get(position);
+//                            Intent i = new Intent(getApplicationContext(), ViewAStreamActivity.class);
+//                            i.putExtra("stream_id", stream_id);
+//                            startActivity(i);
+//                        }
+//                    });
                 } catch (JSONException j) {
                     System.out.println("JSON Error");
                 }
